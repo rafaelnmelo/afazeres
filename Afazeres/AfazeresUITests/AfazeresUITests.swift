d8fbd235ab1aeede80f481ed86bb688317c29f88
Rafael Melo <rafaelnunesmelo@gmail.com>
2024-07-18 13:23:57 -0300

    Teste para tentativa de adicionar item duplicado

--- Afazeres/AfazeresUITests/AfazeresUITests.swift
+++ Afazeres/AfazeresUITests/AfazeresUITests.swift
@@ -35,6 +35,29 @@ class when_user_saves_a_new_task: XCTestCase {
         XCTAssertEqual(1, taskList.cells.count)
     }
     
+    func test_display_error_message_for_duplicate_title_tasks() {
+        let app = XCUIApplication()
+        continueAfterFailure = false
+        app.launch()
+        
+        let titleTextField = app.textFields["titleTextField"]
+        titleTextField.tap()
+        titleTextField.typeText("Mow the lawn")
+        
+        let saveTaskButton = app.buttons["saveTaskButton"]
+        saveTaskButton.tap()
+        
+        titleTextField.tap()
+        titleTextField.typeText("Mow the lawn")
+        saveTaskButton.tap()
+        
+        let taskList = app.tables["taskList"]
+        XCTAssertEqual(1, taskList.cells.count)
+        
+        let messageText = app.staticTexts["messageText"]
+        XCTAssertEqual(messageText.label, "Task is already added")
+    }
+    
     override func tearDown() {
         super.tearDown()
         Springboard.deleteApp()
