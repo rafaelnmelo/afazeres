16d20baa75a3226fe1f744f45aba04753cdb9050
Rafael Melo <rafaelnunesmelo@gmail.com>
2024-07-18 12:57:28 -0300

    Testes de persistencia de dados

--- Afazeres/Afazeres/Managers/CoreDataManager.swift
+++ Afazeres/Afazeres/Managers/CoreDataManager.swift
@@ -6,7 +6,7 @@ class CoreDataManager {
     static let shared: CoreDataManager = CoreDataManager()
     
     private init() {
-        persistentContainer = NSPersistentContainer(name: "TodoAppModel")
+        persistentContainer = NSPersistentContainer(name: "AfazeresAppModel")
         persistentContainer.loadPersistentStores { description, error in
             if let error = error {
                 fatalError("Unable to initialize Core Data \(error)")
